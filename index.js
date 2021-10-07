3825e2166c31ab3a0bd7fb630e5eec4dbcd7f5d6
Chris Marasco <cxm6467@gmail.com>
2021-10-06 23:24:44 -0400

    Added bare bones methods for testing

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+
+/**
+*
+*  GET Request
+*
+**
+app.get('/getTest', function (req, res) => {});
+
+/**
+*
+*  POST Request
+*
+**
+
+app.post('postTest', function (req, res) => {});
+
+/**
+*
+*  Set listen port
+*
+**
+app.list(4040);
+
