d4cc2ad8492157ffbf8525fd8ac04b5db99b312e
Caboose <cxm6467@gmail.com>
2021-10-08 03:16:03 -0400

    Added a response to GET request

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
 const app = express();
 
 //  GET Request
-app.get('/getTest', (req, res) => {});
+app.get('/getTest', (req, res) => {
+    res.json({
+        msg : "This is awesome"
+    });
+});
 
 //  POST Request
 app.post('postTest', (req, res) => {});
