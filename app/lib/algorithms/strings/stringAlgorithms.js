621e063b98222d01294aa37553a43176e46c958e
Chris 'Caboose' Marasco <cxm6467@gmail.com>
2021-10-11 15:24:06 -0400

    Added JSDocs for string algorithms

--- app/lib/algorithms/strings/stringAlgorithms.js
+++ app/lib/algorithms/strings/stringAlgorithms.js
@@ -1,3 +1,10 @@
+/**
+ * Find length of longest palindrome
+ *
+ * @param {string} - string to check
+ * @returns {int} - length of longest palindrome
+ *
+ */
 function longestPalindrome(str) {
   const occurrences = [...Array(128)].map((x) => 0);
   for (const char of str.split('')) {
@@ -17,6 +24,13 @@ function longestPalindrome(str) {
   return length;
 }
 
+/**
+ *  Find first repeating char in a string
+ *
+ *  @param {string} - String to check
+ *  @returns {char} - First repeating char
+ *
+ */
 function firstRepeatingChar(str) {
   const visited = {};
   for (const x of str) {
@@ -26,6 +40,12 @@ function firstRepeatingChar(str) {
   return 'No repeating Chars';
 }
 
+/**
+ * Find longest substr without repeating chars
+ *
+ * @param {string} - String to check for the longest substr
+ * @return {boolean} - check if str has repeating chars
+ */
 function withoutRepeating(str) {
   const visited = [...Array(128)].map((x) => false);
   for (let i = 0; i < str.length; i++) {
@@ -35,13 +55,19 @@ function withoutRepeating(str) {
   return true;
 }
 
+/**
+ * Find longest substr without repeating chars
+ *
+ * @param {string} - String to check for the longest substr
+ * @return {number} - length of longest substr
+ */
+
 function longestSubstringWithoutRepeating(str) {
   let maxLength = 0;
   for (let i = 0; i < str.length; i++) {
     for (let j = i; j < str.length; j++) {
       const substr = str.substring(i, j + 1);
-      if (withoutRepeating(substr) && substr.length > maxLength)
-      {maxLength = substr.length;}
+      if (withoutRepeating(substr) && substr.length > maxLength) { maxLength = substr.length; }
     }
   }
   return maxLength;
