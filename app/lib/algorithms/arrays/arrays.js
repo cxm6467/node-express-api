93b88b4827f9dea504ef20f24ee709cc4f0d55bb
Chris 'Caboose' Marasco <cxm6467@gmail.com>
2021-10-11 00:11:14 -0400

    Added algorithm list for string and array algorithms; Remove 'comma-dangle' rule from eslint; Check to see if algorithm input is valid.

--- app/lib/algorithms/arrays/arrays.js
+++ app/lib/algorithms/arrays/arrays.js
@@ -1,9 +1,23 @@
+const algorithms = [
+  'find_pair',
+  'remove_duplicates',
+  'find_duplicates',
+  'maximum_subarray',
+  'find_peak',
+  'product_except_self'
+];
+
 function arrays(algo, input) {
+  const algoExists = algorithms[algo] === undefined ? false : algorithms[algo];
   const result = {};
 
-  result.algorithm = algo;
-  result.result = input;
+  result.input = input;
 
+  if (!algoExists) {
+    result.result = `[${algo}] is not a valid algorithm...yet.`;
+  } else {
+    result.result = input;
+  }
   return result;
 }
 module.exports = arrays;
--- app/lib/algorithms/strings/strings.js
+++ app/lib/algorithms/strings/strings.js
@@ -1,9 +1,20 @@
+const algorithms = [
+  'first_repeating_character',
+  'without_repeating_character',
+  'longest_substring_without_repeating',
+  'longest_palindrome',
+];
 function strings(algo, input) {
+  const algoExists = algorithms[algo] === undefined ? false : algorithms[algo];
   const result = {};
 
-  result.algorithm = algo;
-  result.result = input;
+  result.input = input;
 
+  if (!algoExists) {
+    result.result = `[${algo}] is not a valid algorithm...yet.`;
+  } else {
+    result.result = input;
+  }
   return result;
 }
 module.exports = strings;
